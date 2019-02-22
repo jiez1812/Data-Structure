6faf5cb7b2e682272403c775d267cf55a0af40c8
Jiez <jieztee@gmail.com>
2019-02-23 00:49:31 +0800

    add inorder

--- TreeNode/TreeNode1.py
+++ TreeNode/TreeNode1.py
@@ -25,10 +25,19 @@ class TreeNode:
         if self.right:
             self.right.printTree()
 
+    #Preorder Traversal : left => root => right
+    def inorderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res += self.inorderTraversal(root.right)
+        return res
+
 tree = TreeNode(52)
 tree.insert(24)
 tree.insert(75)
 tree.insert(12)
 tree.insert(96)
 tree.insert(44)
-tree.printTree()
+print(tree.inorderTraversal(tree))
